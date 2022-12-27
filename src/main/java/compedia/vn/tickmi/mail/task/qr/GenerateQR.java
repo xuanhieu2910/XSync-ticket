67ae9bece185ea95e3c07f2553fd4b44b49aa0cf
HieuVX <hieunx@compedia.vn>
2022-12-27 19:26:36 +0700

    update file path config

--- src/main/java/compedia/vn/tickmi/mail/task/qr/GenerateQR.java
+++ src/main/java/compedia/vn/tickmi/mail/task/qr/GenerateQR.java
@@ -35,6 +35,7 @@ public class GenerateQR {
 
     public static String handlerGeneratePathQR(String ticketEventCode, Long eventId, String nameTicket,
                                                int flatLogo, int flatName, String pathLogo){
+        log.info("Event code : " + ticketEventCode);
         String pathQR = null;
         String root = PropertiesUtil.getProperty("vn.cpa.static.location.upload.gen_qr");
         String filePathOutPut = PropertiesUtil.getProperty("vn.cpa.static.location.export.qr");
