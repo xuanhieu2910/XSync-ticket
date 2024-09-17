87db3914091c6948a846204e6b4a098a07ad6cff
AnhLT <thuchv@compedia.vn>
2024-09-17 23:24:44 +0700

    Update

--- src/main/java/compedia/vn/tickmi/mail/task/ticket/HandleTicketService.java
+++ src/main/java/compedia/vn/tickmi/mail/task/ticket/HandleTicketService.java
@@ -151,7 +151,7 @@ public class HandleTicketService {
                 nameTicket = detail.getNote();
             } else if (detail.getEventId() == 2463) {
                 String stt = registerTicketRepository.getStt(detail.getObjectId());
-                nameTicket = "NSD24-" + String.format("%0" + quantityLength + "d", Long.valueOf(stt));
+                nameTicket = "NSD24-" + String.format("%04d", Long.valueOf(stt));
             }
 
             eventRepository.updateTotalGenTicketEvent(detail.getEventId(), 1);
